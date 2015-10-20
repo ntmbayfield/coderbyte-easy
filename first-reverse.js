a33f71a2b1f726e02660851be4a5e3481525241d
Justin Baize <justinbaize@Justins-MacBook-Air.local>
2015-10-20 10:00:41 -0500

    Add first-reverse to repo

---
+++ first-reverse.js
@@ -0,0 +1,9 @@
+// Have the function FirstReverse(str) take the str parameter being passed
+// and return the string in reversed order.
+
+function FirstReverse(str) { 
+
+	return str.split("").reverse().join("");       
+         
+}
+
