c3a94d04d420fb62801eca8afe3a742551f5e602
Justin Baize <justinbaize@Justins-MacBook-Air.local>
2015-10-21 09:32:18 -0500

    Add 2nd Coderbyte problem

---
+++ first-factorial.js
@@ -0,0 +1,13 @@
+// Have the function FirstFactorial(num) take the num parameter being passed
+// and return the factorial of it (ie. if num = 4, return (4 * 3 * 2 * 1)).
+// For the test cases, the range will be between 1 and 18. 
+
+function FirstFactorial(num) {
+  //base case -- will stop recursion when num === 0 
+  if ( num === 0 ) {
+    return 1;
+  } else {
+    //multiplies num by (num-1)
+    return num * FirstFactorial(num-1);
+  }
+}
