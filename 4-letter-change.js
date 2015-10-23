b947c236c0c51d84732fd5dd85c03059f5b4a58c
Justin Baize <justinbaize@Justins-MacBook-Air.local>
2015-10-23 11:49:12 -0500

    Add working solution and comments

--- 4-letter-change.js
+++ 4-letter-change.js
@@ -4,3 +4,50 @@
 // it in the alphabet (ie. c becomes d, z becomes a). Then
 // capitalize every vowel in this new string (a, e, i, o, u)
 // and finally return this modified string.
+
+//old solution -- can't make any sense of it.
+// function LetterChanges(str) { 
+//   var alphabet = "abcdefghijklmnopqrstuvwxyz";
+//   var newStr = "";
+//   var loc = 0;
+//   for (var i = 0; i < str.length; i++) {
+//     loc = alphabet.indexOf(str[i]);
+//     if (loc === 25) {
+//       newStr = newStr + "a";
+//     } else if (loc === -1) {
+//       newStr = newStr + str[i];
+//     } else {          
+//       newStr = newStr + alphabet[loc + 1];
+//     }
+//   }
+//   return newStr.replace(/[aeiou]/g, function(letter) {return letter.toUpperCase()});        
+// }
+
+function LetterChanges(str) { 
+  //empty string
+  var newString = "";
+
+  //for-loop passing through the string's length
+  for (var i = 0; i < str.length; i++) {
+
+    //var character = the letter at a given index
+    var character = str.charAt(i);
+
+    //if character has matches ranging from (A-Z || a-z) 
+    if (character.match(/[A-Za-z]/) != null)
+
+      //shifts character's charCode by +1
+      character = String.fromCharCode(character.charCodeAt(0)+1);
+
+    //adds the newly shifted character to the empty string
+    newString += character;
+  }
+  //splits newly-populated string on lowercase vowels and substitutes them with their uppercase
+  newString = newString.split("a").join("A")
+    .split("e").join("E")
+    .split("i").join("I")
+    .split("o").join("O")
+    .split("u").join("U");
+
+  return newString; 
+}
