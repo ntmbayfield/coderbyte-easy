5b2ea62b2dc9a4f11482b64a57f7014ad9e4e11c
Jbays <jjbaize@gmail.com>
2015-10-26 15:50:39 -0400

    Add clear comments to code

--- 6-letter-capitalize.js
+++ 6-letter-capitalize.js
@@ -4,16 +4,18 @@ function LetterCapitalize(str) {
   var arrayify = str.split(" ");
   var outputArray = [];
 
-  //for-loop passing along the length of the arrayified str
+  //for-loop passing along the length of arrayify
   for ( var i = 0; i < arrayify.length; i ++ ) {
     //capitalizes the first letter of each word along arrayify's length
     var firstLetter = arrayify[i].charAt(0).toUpperCase();
-    //snips the rest of the 
+    //snips the rest of each word along arrayify's length
     var restOfStringWord = arrayify[i].slice(1);
 
+    //puts capitalized first letter and rest of word together into output array
     outputArray[i] = firstLetter + restOfStringWord;
   }
 
+  //turns outputArray into one string -- output now has same form as input
   return outputArray.join(" "); 
          
 }
