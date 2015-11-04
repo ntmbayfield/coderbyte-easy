a6a21b7bac7ad019e3f17b138aa3c6ceccd112dd
Jbays <jjbaize@gmail.com>
2015-11-04 15:39:12 -0600

    Add clear comments

--- 11-ab-check.js
+++ 11-ab-check.js
@@ -10,13 +10,19 @@
 // Input = "Laura sobs"   ||  Output = "true"
 
 function ABCheck(str) { 
+  //sets detector logic to false
   var passes = false;
+  //for-loop passing through the length of the str argument
   for ( var i = 0; i < str.length; i++ ) {
+    //if str[i] character is "a"
     if ( str.charAt(i).toLowerCase() === "a" ) {
+      //if str[i+4] character is "b"
       if ( str.charAt(i+4).toLowerCase() === "b" ) {
+        //when "b" detected, set detector to true
         passes = true;
       }
     }
   }
+  //return detector
   return passes;     
 }
