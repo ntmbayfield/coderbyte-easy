27a8cf029d915d0e69d806f7e3a0c7c32aa86679
Jbays <jjbaize@gmail.com>
2015-11-05 17:43:14 -0600

    Add 12th problem

---
+++ 12-vowel-count.js
@@ -0,0 +1,15 @@
+// have the function VowelCount(str) take the str string
+// parameter being passed and return the number of vowels
+// the string contains (ie. "All cows eat grass" would
+// return 5). Do not count y as a vowel for this challenge. 
+
+// Correct Sample Output
+// Input = "hello"     ||  Output = 2
+// Input = "coderbyte" ||  Output = 3
+
+function VowelCount(str) { 
+
+  // code goes here  
+  return str; 
+         
+}
