d0e8d1d7bb52e198be24570de9c2f4cef7192a30
Jbays <jjbaize@gmail.com>
2015-11-06 14:28:11 -0600

    Add 13th problem

---
+++ 13-word-count.js
@@ -0,0 +1,15 @@
+// Using the JavaScript language, have the function WordCount(str)
+// take the str string parameter being passed and return the number
+// of words the string contains (ie. "Never eat shredded wheat"
+// would return 4). Words will be separated by single spaces. 
+
+// Correct Sample Output
+// Input = "Hello World"   ||  Output = 2
+// Input = "one 22 three"  ||  Output = 3
+
+function WordCount(str) { 
+
+  // code goes here  
+  return str; 
+         
+}
