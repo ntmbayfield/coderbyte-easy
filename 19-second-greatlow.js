0fce160073263398d64e972dd1c8e59ac90c3638
Jbays <jjbaize@gmail.com>
2015-11-13 11:48:34 -0600

    Add 19th problem

---
+++ 19-second-greatlow.js
@@ -0,0 +1,18 @@
+// have the function SecondGreatLow(arr) take the array of
+// numbers stored in arr and return the second lowest and
+// second greatest numbers, respectively, separated by a
+// space. For example: if arr contains [7, 7, 12, 98, 106]
+// the output should be 12 98. The array will not be empty
+// and will contain at least 2 numbers. It can get tricky
+// if there's just two numbers! 
+
+// Correct Sample Output
+// Input = 1, 42, 42, 180  ||  Output = "42 42"
+// Input = 4, 90           ||  Output = "90 4"
+
+function SecondGreatLow(arr) { 
+
+  // code goes here  
+  return arr; 
+         
+}
