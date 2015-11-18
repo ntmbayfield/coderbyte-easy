8f5607777d070d10615ed0407edd43b44da483d3
Jbays <jjbaize@gmail.com>
2015-11-18 15:47:56 -0600

    Add 23rd problem

---
+++ 23-dash-insert.js
@@ -0,0 +1,15 @@
+// have the function DashInsert(str) insert dashes ('-')
+// between each two odd numbers in str. For example: if
+// str is 454793 the output should be 4547-9-3. Don't
+// count zero as an odd number. 
+
+// Correct Sample Output
+// Input = 99946   ||  Output = 9-9-946
+// Input = 56730   ||  Output = 567-30
+
+function DashInsert(str) { 
+
+  // code goes here  
+  return str; 
+         
+}
