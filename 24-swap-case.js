41cae3dc5573e6012ca3e41a674d52e38788037f
Jbays <jjbaize@gmail.com>
2015-11-19 15:42:15 -0600

    Adding 24th problem

---
+++ 24-swap-case.js
@@ -0,0 +1,16 @@
+// have the function SwapCase(str) take the str
+// parameter and swap the case of each character.
+// For example: if str is "Hello World" the output
+// should be hELLO wORLD. Let numbers and symbols
+// stay the way they are.
+
+// Correct Sample Output
+// Input = "Hello-LOL"    ||  Output = "hELLO-lol"
+// Input = "Sup DUDE!!?"  ||  Output = "sUP dude!!?"
+
+function SwapCase(str) { 
+
+  // code goes here  
+  return str; 
+         
+}
