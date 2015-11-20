871619437ef3a49651a681a4ac1b618322997b22
Jbays <jjbaize@gmail.com>
2015-11-20 17:46:05 -0600

    Finalize comments

--- 25-number-addition.js
+++ 25-number-addition.js
@@ -14,12 +14,18 @@
 
 function NumberAddition(str) {
   var summableArray = [];
+  //replaces all non-digits with space-characters
   var replacer = str.replace(/\D/g, " ");
+  //removes space-characters and wraps all replacer elements in quotes
   var splitter = replacer.split(" ");
+  //removes all empty-string elements in splitter array
   var filtered = splitter.filter(Boolean);
+  //for-loop passing along the length of filtered array
   for ( var i = 0; i < filtered.length; i++ ) {
+    //numberifies elements in filtered array and pushes into output array
     summableArray.push(Number(filtered[i]));
   }
+  //calculates sum of output array
   var sum = summableArray.reduce(function(a,b){ return a+b});
   return sum;
 }
